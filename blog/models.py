182ee05c298c198549d1584fe5be612ee9dad17f
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2022-12-27 14:25:22 +0530

    Designed blog data scheme.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('published','Published')
+        )
+
+    title = models.CharField()
+    slug = models.SlugField()
+    author = models.ForeignKey(to=User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # doc: https://docs.djangoproject.com/en/3.2/ref/models/fields/#choices
+    status = models.CharField(max_length=10,
+                             choices=STATUS_CHOICES,
+                             default='draft')
+    
+    class Meta:
+        ordering = ('-publish')
+    
+    def __str__(self):
+        return self.title
