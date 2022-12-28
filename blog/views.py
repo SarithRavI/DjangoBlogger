0538d56efd46ebeec82b3855ca6b0b8202998bb7
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2022-12-28 16:08:25 +0530

    Modified detail view for comments.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import FormComment
 
 def list_view(request):
     posts = Post.published.all()
@@ -15,5 +16,24 @@ def detail_view(request,year,month,day,post):
                             publish__year = year,
                             publish__month = month,
                             publish__day = day)
+    # get all the comments. 
+    # here we utilize the related_name defined in the Comment model to Post foreign key.                        
+    comments = post.comments.filter(active='active')
+    new_comment = None
+    if request.method == 'POST':
+        form = FormComment(request.post)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        form = FormComment()
+
+    # return render(request,'blog/post/detail.html',
+    #              {'post':post})
+
     return render(request,'blog/post/detail.html',
-                 {'post':post})
+                 {'post':post,
+                 'comments':comments,
+                 'comment_form':form,
+                 'new_comment':new_comment})
