e3f52de51711aade5f792867b9e367701290721b
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2022-12-29 22:57:07 +0530

    Pytest enabled.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
+from django.test import TestCase,Client
 
-# Create your tests here.
+client = Client()
+
+@pytest.mark.django_db
+def test_list_view():
+    response = client.get('/blog/')
+    assert response.status_code == 200 
