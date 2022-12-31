8c8b8ff16ddb9b0e58a8f931e55ee72262b2844e
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2022-12-31 20:29:43 +0530

    Added filtering by tag.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('',list_view,name = 'post_list'),
+    path('tag/<slug:tag_slug>',list_view,name = 'post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',detail_view,name = 'post_detail'),
 ]
